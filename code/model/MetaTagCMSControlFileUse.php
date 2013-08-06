f3fcd2da77abc22c0b207634e552a0cc878ea1d1
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-08-06 02:40:10 +0000

    ADDING File Cleanup
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/metatags/branches/2.4@5040 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/model/MetaTagCMSControlFileUse.php
+++ code/model/MetaTagCMSControlFileUse.php
@@ -1,5 +1,64 @@
 <?php
 
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * SITUATIONS
+ *
+ * 0. A NonFileObject.HasOne File
+ *    example: SiteTree HasOne Image
+ *    type = HAS_ONE
+ *    DataObjectIsFile = false
+ *    FileIsFile = true
+ * 1. A NonFileObject.HasMany Files = see (4)
+ *
+ * 2. A NonFileObject.ManyMany Files
+ *    example: SiteTree ManyMany Images
+ *    type = MANY_MANY
+ *    DataObjectIsFile = false
+ *    FileIsFile = true
+ *
+ * 3. A NonFileObject.BelongsManyMany Files
+ *    example: SiteTree BELONGS_MANY_MANY Images
+ *    type = BELONGS_MANY_MANY
+ *    DataObjectIsFile = false
+ *    FileIsFile = true
+ *
+ * 4. A File.HasOne NonFileObject
+ *    example: Image HasOne SiteTree
+ *    type = HAS_ONE
+ *    DataObjectIsFile = false
+ *    FileIsFile = true
+ *
+ * 5. A File.HasMany NonFileObjects = see 1
+ *
+ * 6. A File.HasOne AnotherFile
+ *    example: Image HAS_ONE Images
+ *    type = BELONGS_MANY_MANY
+ *    DataObjectIsFile = true
+ *    FileIsFile = true
+ *
+ * 7. A File.HasMany Files = see (6)
+ *
+ * 8. A File.ManyMany Files
+ *    example: Image MANY_MANY Images
+ *    type = MANY_MANY
+ *    DataObjectIsFile = true
+ *    FileIsFile = true
+ *
+ * 9. A File.BelongsManyMany Files
+ *    example: Image MANY_MANY Images
+ *    type = BELONGS_MANY_MANY
+ *    DataObjectIsFile = true
+ *    FileIsFile = true
+ */
 
 class MetaTagCMSControlFileUse extends DataObject {
 
@@ -44,18 +103,17 @@ class MetaTagCMSControlFileUse extends DataObject {
 		"DataObjectIsFile" => "Boolean",
 		"FileIsFile" => "Boolean",
 		"IsLiveVersion" => "Boolean",
-		"ConnectionType" => "Enum('DB,HAS_ONE,HAS_MANY,MANY_MANY,BELONGS_MANY_MANY')"
+		"ConnectionType" => "Enum('DB,HAS_ONE,MANY_MANY,BELONGS_MANY_MANY')"
 	);
 
 
 
 	/**
 	 * create a list of tables and fields that need to be checked
+	 * see class comments
 	 */
 	function requireDefaultRecords() {
 		self::$file_classes = ClassInfo::subclassesFor("File");
-		print_r(self::$file_classes);
-		die("boo");
 		parent::requireDefaultRecords();
 		//start again
 		DB::query("DELETE FROM \"MetaTagCMSControlFileUse\";");
@@ -108,7 +166,20 @@ class MetaTagCMSControlFileUse extends DataObject {
 			$hasManyArray = $newItems; //isset($hasManyArray) ? array_merge($newItems, (array)$hasManyArray) : $newItems;
 			if($hasManyArray && count($hasManyArray)) {
 				foreach($hasManyArray as $fieldName => $hasManyClass) {
-					$this->createNewRecord($class, $fieldName, $hasManyClass, "HAS_MANY");
+					//NOTE - We are referencing HAS_ONE here on purpose!!!!
+					$hasManyCheckItems = (array) Object::inherited_static($hasManyClass, 'has_one');
+					$hasManyFound = false;
+					foreach($hasManyCheckItems as $hasManyfieldName => $hasManyForeignClass) {
+						if($hasManyForeignClass == $class) {
+							$this->createNewRecord($hasManyClass, $hasManyfieldName, $hasManyForeignClass, "HAS_ONE");
+							$hasManyFound = true;
+						}
+					}
+					//now we have to guess!
+					if(!$hasManyFound) {
+						$this->createNewRecord($hasManyClass, $class, $class, "HAS_ONE");
+						$hasManyFound = true;
+					}
 				}
 			}
 			//many many
@@ -129,39 +200,55 @@ class MetaTagCMSControlFileUse extends DataObject {
 	}
 
 	private function createNewRecord($dataObjectClassName, $dataObjectFieldName, $fileClassName, $connectionType) {
+		//exceptions....
 		if(in_array($dataObjectClassName, self::$excluded_classes)  || in_array($fileClassName, self::$excluded_classes)) {
 			return;
 		}
-		//if( ! in_array($fileClassName, self::$file_classes) && $connectionType != "DB") {
-		//	return;
-		//}
 		if($dataObjectClassName == "SiteTree" && $dataObjectFieldName == "ImageTracking") {
 			return;
 		}
-		if( ! DB::query("
-			SELECT COUNT(*)
-			FROM \"MetaTagCMSControlFileUse\"
-			WHERE \"DataObjectClassName\" = '$dataObjectClassName' AND  \"DataObjectFieldName\" = '$dataObjectFieldName' AND \"FileClassName\" = '$fileClassName'
-		")->value()) {
-			$obj = new MetaTagCMSControlFileUse();
-			$obj->DataObjectClassName = $dataObjectClassName;
-			$obj->DataObjectFieldName = $dataObjectFieldName;
-			$obj->FileClassName = $fileClassName;
-			$obj->ConnectionType = $connectionType;
-			$obj->DataObjectIsFile = in_array($dataObjectClassName, self::$file_classes);
-			$obj->FileIsFile = in_array($fileClassName, self::$file_classes);
-			$obj->IsLiveVersion = 0;
-			$obj->write();
-			if(ClassInfo::is_subclass_of($dataObjectClassName, "SiteTree")) {
-				$obj = new MetaTagCMSControlFileUse();
-				$obj->DataObjectClassName = $dataObjectClassName."_Live";
-				$obj->DataObjectFieldName = $dataObjectFieldName;
-				$obj->FileClassName = $fileClassName;
-				$obj->ConnectionType = $connectionType;
-				$obj->IsLiveVersion = 1;
-				$obj->write();
+
+		//at least one of them is a file...
+		if( in_array($dataObjectClassName, self::$file_classes) || in_array($fileClassName, self::$file_classes)) {
+			if( ! DB::query("
+				SELECT COUNT(*)
+				FROM \"MetaTagCMSControlFileUse\"
+				WHERE \"DataObjectClassName\" = '$dataObjectClassName' AND  \"DataObjectFieldName\" = '$dataObjectFieldName' AND \"FileClassName\" = '$fileClassName'
+			")->value()) {
+				$dataObjectIsFile =  in_array($dataObjectClassName, self::$file_classes) ? 1 : 0;
+				$fileIsFile =  in_array($fileClassName, self::$file_classes) ? 1 : 0;
+				for($i = 0 $i < ($dataObjectIsFile + $fileIsFile); $i++) {
+					$computedDataObjectIsFile = false;
+					$computedFileIsFile = false;
+					if($i == 0 && $dataObjectIsFile) {
+						$computedDataObjectIsFile = true;
+					}
+					if(($i == 1 && $fileIsFile ) || !$dataObjectIsFile) {
+						$computedFileIsFile = true;
+					}
+					$obj = new MetaTagCMSControlFileUse();
+					$obj->DataObjectClassName = $dataObjectClassName;
+					$obj->DataObjectFieldName = $dataObjectFieldName;
+					$obj->FileClassName = $fileClassName;
+					$obj->ConnectionType = $connectionType;
+					$obj->DataObjectIsFile = $computedDataObjectIsFile;
+					$obj->FileIsFile =  $computedFileIsFile;
+					$obj->IsLiveVersion = 0;
+					$obj->write();
+				}
+				elseif(ClassInfo::is_subclass_of($dataObjectClassName, "SiteTree")) {
+					$obj = new MetaTagCMSControlFileUse();
+					$obj->DataObjectClassName = $dataObjectClassName."_Live";
+					$obj->DataObjectFieldName = $dataObjectFieldName;
+					$obj->FileClassName = $fileClassName;
+					$obj->ConnectionType = $connectionType;
+					$obj->DataObjectIsFile = $computedDataObjectIsFile;
+					$obj->FileIsFile =  $computedFileIsFile;
+					$obj->IsLiveVersion = 1;
+					$obj->write();
+				}
+				DB::alteration_message("creating new MetaTagCMSControlFileUse: $dataObjectClassName, $dataObjectFieldName, $fileClassName, $connectionType");
 			}
-			DB::alteration_message("creating new MetaTagCMSControlFileUse: $dataObjectClassName, $dataObjectFieldName, $fileClassName, $connectionType");
 		}
 	}
 
@@ -201,92 +288,117 @@ class MetaTagCMSControlFileUse extends DataObject {
 			$checks = DataObject::get("MetaTagCMSControlFileUse");
 			if($checks) {
 				foreach($checks as $check) {
-					$sql = "";
-					switch ($check->ConnectionType) {
-						case "DB":
-							$fileName = $file->Name;
-							$sql = "
-								SELECT COUNT(\"{$check->DataObjectClassName}\".\"ID\")
-								FROM \"{$check->DataObjectClassName}\"
-								WHERE LOCATE('$fileName', \"{$check->DataObjectClassName}\".\"{$check->DataObjectFieldName}\") > 0
-							";
-							if($saveListOfPlaces) {
-								$sqlListOfPlaces = "
-									SELECT \"{$check->DataObjectClassName}\".\"ID\" AS MyID
+						for($i = 0; $i < 2; $i++) {
+						$sql = "";
+						switch ($check->ConnectionType) {
+							case "DB":
+								$fileName = $file->Name;
+								$sql = "
+									SELECT COUNT(\"{$check->DataObjectClassName}\".\"ID\")
 									FROM \"{$check->DataObjectClassName}\"
 									WHERE LOCATE('$fileName', \"{$check->DataObjectClassName}\".\"{$check->DataObjectFieldName}\") > 0
 								";
-								$objectNameListOfPlaces = $check->DataObjectClassName;
-							}
-							break;
-
-						case "HAS_ONE":
-							$sql = "
-								SELECT COUNT(\"{$check->DataObjectClassName}\".\"ID\")
-								FROM \"{$check->DataObjectClassName}\"
-								WHERE \"{$check->DataObjectFieldName}ID\" = {$fileID};
-							";
-							if($saveListOfPlaces) {
-								$sqlListOfPlaces = "
-									SELECT \"{$check->DataObjectClassName}\".\"ID\" AS MyID
-									FROM \"{$check->DataObjectClassName}\"
-									WHERE \"{$check->DataObjectFieldName}ID\" = {$fileID};
-								";
-								$objectNameListOfPlaces = $check->DataObjectClassName;
-							}
-							break;
-
-						case "HAS_MANY":
-							$sql = "
-								SELECT COUNT(\"{$check->DataObjectClassName}\".\"ID\")
-								FROM \"{$check->DataObjectClassName}\"
-									INNER JOIN  {$check->FileClassName}
-										ON \"{$check->DataObjectClassName}\".\"{$check->FileClassName}ID\" = \"{$check->FileClassName}\".\"ID\"
-								WHERE \"{$check->FileClassName}\".\"ID\" = {$fileID};
-							";
-							if($saveListOfPlaces) {
-								$sqlListOfPlaces = "
-									SELECT \"{$check->DataObjectClassName}\".\"ID\"  AS MyID
-									FROM \"{$check->DataObjectClassName}\"
-										INNER JOIN  {$check->FileClassName}
-											ON \"{$check->DataObjectClassName}\".\"{$check->FileClassName}ID\" = \"{$check->FileClassName}\".\"ID\"
-									WHERE \"{$check->FileClassName}\".\"ID\" = {$fileID};
-								";
-								$objectNameListOfPlaces = $check->DataObjectClassName;
-							}
-							break;
-						case "MANY_MANY":
-						case "BELONGS_MANY_MANY":
-							$sql = "
-								SELECT COUNT(\"{$check->DataObjectClassName}_{$check->DataObjectFieldName}\".\"ID\")
-								FROM \"{$check->DataObjectClassName}_{$check->DataObjectFieldName}\"
-								WHERE \"{$check->FileClassName}ID\" = $fileID;
-							";
-							if($saveListOfPlaces) {
-								$sqlListOfPlaces = "
-									SELECT \"{$check->DataObjectClassName}_{$check->DataObjectFieldName}\".\"{$check->DataObjectClassName}ID\"  AS MyID
-									FROM \"{$check->DataObjectClassName}_{$check->DataObjectFieldName}\"
-									WHERE \"{$check->FileClassName}ID\" = $fileID;
-								";
-								$objectNameListOfPlaces = $check->DataObjectClassName;
-							}
-							break;
-					}
-					$result = DB::query($sql, false);
-					$count = $result->value();
-					if($count) {
-						if($quickBooleanCheck) {
-							return true;
-						}
-						else {
-							if($saveListOfPlaces) {
-								if( ! $check->IsLiveVersion) {
-									self::list_of_places_adder($fileID, $sqlListOfPlaces, $objectNameListOfPlaces);
-									$sqlListOfPlaces = "";
-									$objectNameListOfPlaces = "";
+								if($saveListOfPlaces) {
+									$sqlListOfPlaces = "
+										SELECT \"{$check->DataObjectClassName}\".\"ID\" AS MyID
+										FROM \"{$check->DataObjectClassName}\"
+										WHERE LOCATE('$fileName', \"{$check->DataObjectClassName}\".\"{$check->DataObjectFieldName}\") > 0
+									";
+									$objectNameListOfPlaces = $check->DataObjectClassName;
 								}
+								break;
+
+							case "HAS_ONE":
+								$countSelect = "SELECT COUNT(\"{$check->DataObjectClassName}\".\"ID\")"
+								$listSelect = "SELECT \"{$check->DataObjectClassName}\".\"ID\" AS MyID"
+								$from = "FROM \"{$check->DataObjectClassName}\""
+								if($check->FileIsFile) {
+									$where = "WHERE \"{$check->DataObjectFieldName}ID\" = {$fileID}"
+									$sql = "
+										$countSelect
+										$from
+										$where;
+									";
+									if($saveListOfPlaces) {
+										$sqlListOfPlaces = "
+											$listSelect
+											$from
+											$where;
+										";
+									}
+								}
+								elseif($check->DataObjectIsFile) {
+									$where = "WHERE \"{$check->DataObjectFieldName}ID\" > 0 AND \"{$check->DataObjectClassName}\".\"ID\" = {$fileID}";
+									$sql = "
+										$countSelect
+										$from
+										$where;
+									";
+									if($saveListOfPlaces) {
+										$sqlListOfPlaces = "
+											$listSelect
+											$from
+											$where;
+										";
+									}
+									$objectNameListOfPlaces = $check->DataObjectClassName;
+								}
+								break;
+							case "MANY_MANY":
+							case "BELONGS_MANY_MANY":
+								$countSelect = "SELECT COUNT(\"{$check->DataObjectClassName}_{$check->DataObjectFieldName}\".\"ID\")";
+								$listSelect = "SELECT \"{$check->DataObjectClassName}_{$check->DataObjectFieldName}\".\"{$check->DataObjectClassName}ID\" AS MyID";
+								$from = "FROM \"{$check->DataObjectClassName}_{$check->DataObjectFieldName}\"";
+								if($this->FileIsFile) {
+									$where = "WHERE \"{$check->FileClassName}ID\" = $fileID;";
+									$sql = "
+										$countSelect
+										$from
+										$where;
+									";
+									if($saveListOfPlaces) {
+										$sqlListOfPlaces = "
+											$listSelect
+											$from
+											$where
+										";
+										$objectNameListOfPlaces = $check->FileClassName;
+									}
+								}
+								elseif($check->DataObjectIsFile) {
+									$where = "WHERE \"{$check->DataObjectClassName}ID\" = $fileID;";
+									$sql = "
+										$countSelect
+										$from
+										$where;
+									";
+									if($saveListOfPlaces) {
+										$sqlListOfPlaces = "
+											$listSelect
+											$from
+											$where
+										";
+										$objectNameListOfPlaces = $check->FileClassName;
+									}
+								}
+								break;
+						}
+						$result = DB::query($sql, false);
+						$count = $result->value();
+						if($count) {
+							if($quickBooleanCheck) {
+								return true;
 							}
-							self::$file_usage_array[$fileID] += $count;
+							else {
+								if($saveListOfPlaces) {
+									if( ! $check->IsLiveVersion) {
+										self::list_of_places_adder($fileID, $sqlListOfPlaces, $objectNameListOfPlaces);
+										$sqlListOfPlaces = "";
+										$objectNameListOfPlaces = "";
+									}
+								}
+								self::$file_usage_array[$fileID] += $count;
+							}
 						}
 					}
 				}
@@ -451,12 +563,6 @@ class MetaTagCMSControlFileUse extends DataObject {
 								$where = "\"{$check->DataObjectFieldName}ID\" = {$fileID}";
 								$innerJoinTable = "";
 								$innerJoinJoin = "";
-								break;
-							case "HAS_MANY":
-								$objName = $check->DataObjectClassName;
-								$where = "\"{$check->DataObjectClassName}\".\"ID\" = {$fileID}";
-								$innerJoinTable = "$check->FileClassName";
-								$innerJoinJoin = "\"{$check->DataObjectClassName}\".\"{$check->FileClassName}ID\" = \"{$check->FileClassName}\".\"ID\"";
 								break;
 							case "BELONGS_MANY_MANY":
 							case "MANY_MANY":
